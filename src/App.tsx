04631c4c6422e826d0d570ba2df7a29f3be8859e
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-26 04:38:22 +0200

    Added voting and highscore tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
 import { useEffect, useState } from "react"
+import { useRecoilState } from "recoil";
 import {CurrentPanel, VotePanel} from "./components/VotePanel";
+import { scoreState } from "./recoil";
 import { Anime } from "./types/anilist";
 import { fetchRandomAnime } from "./utils/animeUtil";
 
 function App() {
+    const [gameState, setGameState] = useRecoilState(scoreState);
+    const [score, setScore] = useState(0);
     const [left, setLeft] = useState<Anime|null>(null);
     const [right, setRight] = useState<Anime|null>(null);
 
     useEffect(() => {
+        if (!gameState) {
+            setGameState({ highscore: score });
+            localStorage.setItem("anime-hilo-state", JSON.stringify(gameState));
+        }
         // Fetch two on first load
         fetchRandomAnime().then(data => setLeft(data));
         fetchRandomAnime().then(data => setRight(data));
     }, []);
+
+    useEffect(() => {
+        if (!gameState) return;
+        if (score > gameState.highscore) {
+            setGameState({highscore: score})
+        }
+    }, [score]);
+
+    useEffect(() => {
+        localStorage.setItem("anime-hilo-state", JSON.stringify(gameState));
+    }, [gameState]);
 
     if (!left || !right) {
         return (
@@ -21,13 +40,33 @@ function App() {
         )
     }
 
+    function onVote(vote: number) {
+        if (!left || !right) return;
+
+        if ((vote == 1 && right.popularity >= left.popularity) || (vote == -1 && right.popularity <= left.popularity)) {
+            fetchRandomAnime().then(data => {
+                setLeft(right);
+                setRight(data);
+                setScore(score + 1);
+            });
+            return;
+        }
+
+        fetchRandomAnime().then(data => setLeft(data));
+        fetchRandomAnime().then(data => setRight(data));
+        setScore(0);
+    }
+
     return (
         <div className="flex w-full h-full relative bg-gray-700">
             <CurrentPanel anime={left}/>
-            <div className="flex items-center justify-center rounded-full bg-gray-700 text-yellow-200 text-3xl font-bold text-black h-[6.25rem] w-[6.25rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[9999]">
+            <div className="flex items-center justify-center rounded-full bg-gray-700 text-yellow-200 text-3xl font-bold h-[6.25rem] w-[6.25rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[9999]">
                 VS
             </div>
-            <VotePanel anime={right}/>
+            <VotePanel anime={right} onVote={onVote}/>
+            {/* Score Panel */}
+            <div className="bg-gray-800 text-white p-4 absolute bottom-0 left-0">High Score: {gameState?.highscore ?? 0}</div>
+            <div className="bg-gray-800 text-white p-4 absolute bottom-0 right-0">Score: {score}</div>
         </div>
     )
 }
--- src/components/VotePanel.tsx
+++ src/components/VotePanel.tsx
@@ -13,17 +13,17 @@ export function CurrentPanel({anime}: {anime: Anime}) {
     )
 }
 
-export function VotePanel({anime}: {anime: Anime}) {
+export function VotePanel({anime, onVote}: {anime: Anime, onVote: (vote: number) => void}) {
     return (
         <div className="flex flex-col text-white justify-center items-center bg-center bg-cover w-full h-[100vh] relative" style={{backgroundImage: `url(${anime.coverImage.extraLarge})`}}>
             <div className="bg-black opacity-[0.7] h-full w-full absolute"></div>
             <div className="flex flex-col gap-[10px] z-[999] text-center items-center">
                 <h1 className="text-3xl font-bold">{anime.title.userPreferred}</h1>
                 <span>is</span>
-                <button className="bg-transparent border-2 px-2 py-3 w-[12.5rem] text-yellow-200 font-bold border-solid border-white rounded-full hover:bg-white hover:text-black transition">
+                <button onClick={() => onVote(1)} className="bg-transparent border-2 px-2 py-3 w-[12.5rem] text-yellow-200 font-bold border-solid border-white rounded-full hover:bg-white hover:text-black transition">
                     Higher
                 </button>
-                <button className="bg-transparent border-2 px-2 py-3 w-[12.5rem] text-yellow-200 font-bold border-solid border-white rounded-full hover:bg-white hover:text-black transition">
+                <button onClick={() => onVote(-1)} className="bg-transparent border-2 px-2 py-3 w-[12.5rem] text-yellow-200 font-bold border-solid border-white rounded-full hover:bg-white hover:text-black transition">
                     Lower
                 </button>
             </div>
