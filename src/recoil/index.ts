248b23b36a397e08951ed5d44cbf0e86df008afd
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-26 04:38:02 +0200

    Updated game state

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
+import { GameState } from "../types/game";
 
-export const scoreState = atom({
+export const scoreState = atom<GameState|null>({
     key: "anime-hilo-state",
-    default: localStorage.getItem("anime-hilo-state")
+    default: localStorage.getItem("anime-hilo-state") ? JSON.parse(localStorage.getItem("anime-hilo-state")!) : null
 });
--- src/types/game.d.ts
+++ src/types/game.d.ts
@@ -1,4 +1,3 @@
 export interface GameState {
     highscore: number;
-    score: number;
 }
