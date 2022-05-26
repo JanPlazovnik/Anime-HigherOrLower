330228e85979f7af82095717d0b07f46940095df
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-26 04:05:45 +0200

    Updated styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import VotePanel from "./components/VotePanel";
+import {CurrentPanel, VotePanel} from "./components/VotePanel";
 import { Anime } from "./types/anilist";
 import { fetchRandomAnime } from "./utils/animeUtil";
 
@@ -14,12 +14,19 @@ function App() {
     }, []);
 
     if (!left || !right) {
-        return (<h1>Loading</h1>)
+        return (
+            <div className="flex w-full h-full relative text-6xl w-full h-[100vh] justify-center items-center text-white bg-gray-700">
+                Loading...
+            </div>
+        )
     }
 
     return (
-        <div className="flex w-full h-full">
-            <VotePanel anime={left}/>
+        <div className="flex w-full h-full relative bg-gray-700">
+            <CurrentPanel anime={left}/>
+            <div className="flex items-center justify-center rounded-full bg-gray-700 text-yellow-200 text-3xl font-bold text-black h-[6.25rem] w-[6.25rem] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[9999]">
+                VS
+            </div>
             <VotePanel anime={right}/>
         </div>
     )
