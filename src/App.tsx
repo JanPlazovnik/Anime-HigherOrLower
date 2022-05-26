7e24d28d84e4e083150856ae9fd54dd7611f48c1
Jan Plazovnik <jan.plazovnik@gmail.com>
2022-05-26 03:42:21 +0200

    Displayed the two fetched anime

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react"
+import VotePanel from "./components/VotePanel";
+import { Anime } from "./types/anilist";
+import { fetchRandomAnime } from "./utils/animeUtil";
+
 function App() {
-    
+    const [left, setLeft] = useState<Anime|null>(null);
+    const [right, setRight] = useState<Anime|null>(null);
+
+    useEffect(() => {
+        // Fetch two on first load
+        fetchRandomAnime().then(data => setLeft(data));
+        fetchRandomAnime().then(data => setRight(data));
+    }, []);
+
+    if (!left || !right) {
+        return (<h1>Loading</h1>)
+    }
+
     return (
-        <div className="App">
-            <h1>yo</h1>
+        <div className="flex w-full h-full">
+            <VotePanel anime={left}/>
+            <VotePanel anime={right}/>
         </div>
     )
 }
